74033b24841aec49fe622787dbcd5b32801e06b4
shexiaoheng <shexiaoheng@163.com>
2020-09-24 16:19:47 +0800

    first commit

---
+++ app/src/main/java/com/heng/jenkinstest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.heng.jenkinstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
